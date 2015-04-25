0aa4d49903356b5b1ba523a44400e3ba49d3e126
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-04-25 18:48:04 +0800

    icon cache

---
+++ rebuild_icon_cache.bat
@@ -0,0 +1,28 @@
+rem close Windows shell program explorer
+
+taskkill /f /im explorer.exe
+
+rem clean up system Icon Cache
+
+attrib -h -s -r "%userprofile%\AppData\Local\IconCache.db"
+
+del /f "%userprofile%\AppData\Local\IconCache.db"
+
+attrib /s /d -h -s -r "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*"
+
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_96.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_102.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_256.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_1024.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_idx.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_sr.db"
+
+rem clean up system trays icons
+
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v IconStreams
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v PastIconsStream
+
+rem restart Windows shell program explorer
+
+start explorer 
